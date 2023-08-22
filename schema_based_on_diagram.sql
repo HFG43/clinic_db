6a24c742cf92a4526cbf415be371b9a15d1a7817
Andrea Manuel <andreamanuel2207@gmail.com>
2023-08-22 15:45:31 -0600

    Create index for all the foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,15 @@ CREATE TABLE treatments (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, type V
 CREATE TABLE invoice_items (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, unit_price DECIMAL(8,2), quantity INT, total_price DECIMAL(8,2), invoice_id INT, treatment_id INT, CONSTRAINT fk_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON UPDATE CASCADE, CONSTRAINT fk_treatments_id FOREIGN KEY (treatment_id) REFERENCES treatments(id) ON UPDATE CASCADE);
 
 CREATE TABLE treatments_history (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, medical_histories_id INT, treatments_id INT, CONSTRAINT fk_med_hist FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id) ON UPDATE CASCADE, CONSTRAINT fk_treats FOREIGN KEY (treatments_id) REFERENCES treatments(id) ON UPDATE CASCADE);
+
+CREATE INDEX index_patient_id ON medical_histories(patient_id);
+
+CREATE INDEX index_medical_history_id ON invoices(medical_history_id);
+
+CREATE INDEX index_invoice_id ON invoice_items(invoice_id);
+
+CREATE INDEX index_treatment_id ON invoice_items(treatment_id);
+
+CREATE INDEX index_medical_histories_id ON treatments_history(medical_histories_id);
+
+CREATE INDEX index_treatments_id ON treatments_history(treatments_id);
